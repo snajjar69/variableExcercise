c201524825b04e5c843fa85fa720a553560b4efb
snajjar69 <snajjar69@gmail.com>
2024-07-12 15:20:59 -0400

    Added variable Exercise

---
+++ variableExcercise/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace variableExcercise;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string dogName = "Ralph";
+        int dogAge = 10;
+        
+        Console.WriteLine($"My dog's name is Ralph, He is 10 years old");
+    }
+}
