a4a821893f6c7fcaddb380e51ce54e0c5883ece1
snajjar69 <snajjar69@gmail.com>
2024-07-15 09:51:28 -0400

    corrected variable

--- variableExcercise/Program.cs
+++ variableExcercise/Program.cs
@@ -4,9 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        string dogName = "Ralph";
-        int dogAge = 10;
+        //Variable Declarations
+        string dogName;
+        int dogAge;
+        bool isHungry;
         
-        Console.WriteLine($"My dog's name is Ralph, He is 10 years old");
+        //Variable Initializations
+        dogName = "Rex";
+        dogAge = 12;
+        isHungry = true;
+        double dogWeight = 67.8; //Decimals
+        decimal dogHeight = 19.4m; // ends with m
+        
+       
+        Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old." +
+                          $"He is {dogWeight} lbs, and {dogHeight} inches tall");
+
+        Console.WriteLine($"It is {isHungry} that he is always hungry."); //String Interportation
     }
 }
